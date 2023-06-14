9a9ad4229d73c71359049f30476e82f821c1aaf9
harsh7739 <hrbhumi123@gmail.com>
2023-06-14 23:58:54 +0530

    added

---
+++ wood-watch/src/component/Login.jsx
@@ -0,0 +1,10 @@
+function Login(){
+    return (
+        <>
+        <h1>Login Page</h1>
+        <link to="/">Home</link>
+        <link to="/login">Login</link>
+        </>
+    )
+}
+export default Login
